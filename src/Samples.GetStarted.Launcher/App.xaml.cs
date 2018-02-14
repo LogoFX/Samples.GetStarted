f01c838eee226ae23839e03a69cd725a42fbca32
Gennady Verdel <godrose@gmail.com>
2018-02-14 18:35:30 +0200

    Cleaned up all unneeded projects to create skeleton

--- src/Samples.GetStarted.Launcher/App.xaml.cs
+++ src/Samples.GetStarted.Launcher/App.xaml.cs
@@ -1,21 +1,11 @@
-﻿using LogoFX.Client.Bootstrapping;
-using LogoFX.Client.Mvvm.Commanding;
-using LogoFX.Client.Mvvm.ViewModel.Services;
-using LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer;
-
-namespace Samples.GetStarted.Launcher
+﻿namespace Samples.GetStarted.Launcher
 {
     partial class App
     {
         public App()
         {            
             var bootstrapper = new AppBootstrapper();
-            bootstrapper
-                .UseResolver()   
-                .UseCommanding()
-                .UseViewModelCreatorService()
-                .UseViewModelFactory()
-                .Initialize();            
+            bootstrapper.Initialize();            
         }
     }
 }
