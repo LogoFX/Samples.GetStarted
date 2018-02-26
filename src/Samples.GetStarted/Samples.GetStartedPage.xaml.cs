b3de0145e13636912e65d3c61e7e0899f844eb7a
Gennady Verdel <godrose@gmail.com>
2018-02-26 08:19:07 +0200

    Creating mobile solution from VS template

---
+++ src/Samples.GetStarted/Samples.GetStartedPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Samples.GetStarted
+{
+    public partial class Samples_GetStartedPage : ContentPage
+    {
+        public Samples_GetStartedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
