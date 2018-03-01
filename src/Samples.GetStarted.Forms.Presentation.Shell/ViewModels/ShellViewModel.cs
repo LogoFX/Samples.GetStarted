6171ea9adf833af2e11031463714dcdd0f3ef66c
Gennady Verdel <godrose@gmail.com>
2018-03-01 08:51:34 +0200

    Copying the code from mvvm.notifications - works without weak delegates

--- src/Samples.GetStarted.Forms.Presentation.Shell/ViewModels/ShellViewModel.cs
+++ src/Samples.GetStarted.Forms.Presentation.Shell/ViewModels/ShellViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using LogoFX.Client.Mvvm.Commanding;
 using LogoFX.Client.Mvvm.ViewModel.Extensions;
+using LogoFX.Core;
 using Samples.Client.Model.Contracts;
 
 namespace Samples.GetStarted.Forms.Presentation.Shell.ViewModels
@@ -11,7 +15,15 @@ namespace Samples.GetStarted.Forms.Presentation.Shell.ViewModels
         public ShellViewModel(IDataService dataService)
             : base(dataService.SingleItem)
         {
-            
+            Model.NotifyOn("IsDirty", (o, o1) => NotifyOfPropertyChange(() => IsDirty));
+            Model.NotifyOn("CanCancelChanges", (o, o1) => NotifyOfPropertyChange(() => CanCancelChanges));          
+            this.PropertyChanged += (object sender, System.ComponentModel.PropertyChangedEventArgs e) => 
+            {
+                if (e.PropertyName == "IsDirty" || e.PropertyName == "CanCancelChanges")
+                {
+                    
+                }
+            };
         }
 
         protected override async Task<bool> SaveMethod(IWarehouseItem model)
@@ -37,24 +49,44 @@ namespace Samples.GetStarted.Forms.Presentation.Shell.ViewModels
         }));
     }
 
-    /*
-    public static class TempExtensions
+    public static class NotificationsHelper
     {
-        internal static T MyEx<T>(this T command, INotifyPropertyChanged notifiable, Expression<Func<object>> propertySelector)
-        where
-                T : ICommand, IReceiveEvent
+        private static readonly WeakKeyDictionary<object, Dictionary<string, Action<object, object>>> _notifiers =
+            new WeakKeyDictionary<object, Dictionary<string, Action<object, object>>>();
+        
+        /// <summary>
+        /// Subscribes supplied object to property changed notifications and invokes the provided callback        
+        /// </summary>
+        /// <typeparam name="T">Type of subject</typeparam><param name="subject">Subject</param><param name="path">Property path</param><param name="callback">Notification callback</param>
+        public static void NotifyOn<T>(this T subject, string path, Action<object, object> callback)
+          where T : INotifyPropertyChanged
         {
-            Observable
-                .FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>((a) => notifiable.PropertyChanged += a, (a) => notifiable.PropertyChanged -= a)
-                .Where(a => 
+            if (_notifiers.TryGetValue(subject, out var dictionary) == false)
             {
-                var propName = propertySelector.GetPropertyName();
-                return a.EventArgs.PropertyName == propName;   
-                })
-                .Subscribe(a => command.ReceiveWeakEvent(a.EventArgs));
+                _notifiers.Add(subject, dictionary = new Dictionary<string, Action<object, object>>());
+            }
 
-            return command;
+            dictionary.Remove(path);
+                       
+            subject.PropertyChanged += (sender, propertyChangedEventArgs) =>
+            {
+                if (propertyChangedEventArgs.PropertyName == path)
+                {
+                    dictionary[path]?.Invoke(new object(), new object());
+                }
+            };
+            dictionary.Add(path, callback);
+        }
+
+        /// <summary>
+        /// Unsubscribes supplied object from property changed notifications        
+        /// </summary>
+        /// <typeparam name="T">Type of subject</typeparam><param name="subject">Subject</param><param name="path">Property path</param>
+        public static void UnNotifyOn<T>(this T subject, string path)
+        {
+            if (!_notifiers.TryGetValue(subject, out var dictionary) || !dictionary.ContainsKey(path))
+                return;
+            dictionary.Remove(path);
         }
     }
-    */
 }
